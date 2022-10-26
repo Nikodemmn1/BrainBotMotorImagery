104d581f841c46f651df6be4e60d2762cbf5ccad
nikodemmn1 <nikodemmn1@gmail.com>
2022-10-26 22:38:28 +0200

    Improved reporting in tensorboard, batch size increased, architecture changed.

--- train_torch.py
+++ train_torch.py
@@ -1,6 +1,6 @@
 from torch.utils.data import DataLoader
 from pytorch_lightning import Trainer
-from pytorch_lightning.callbacks import TQDMProgressBar, StochasticWeightAveraging
+from pytorch_lightning.callbacks import TQDMProgressBar, StochasticWeightAveraging, ModelCheckpoint
 from Models.OneDNet import OneDNet
 from Dataset.dataset import *
 
@@ -14,17 +14,25 @@ def main():
                               "./Data/EEGLarge/Test/Test.npy",
                               included_classes, included_channels)
     train_dataset, val_dataset, test_dataset = full_dataset.get_subsets()
-    train_data = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=12)
-    val_data = DataLoader(val_dataset, batch_size=8, shuffle=False, num_workers=0)
+    train_data = DataLoader(train_dataset, batch_size=512, shuffle=True, num_workers=12)
+    val_data = DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=0)
+    test_data = DataLoader(test_dataset, batch_size=1, shuffle=False, num_workers=0)
 
     model = OneDNet(len(included_channels), included_classes, train_dataset.indices,
                     val_dataset.indices, test_dataset.indices)
-    trainer = Trainer(gpus=-1, callbacks=[TQDMProgressBar(refresh_rate=100), StochasticWeightAveraging(swa_lrs=1e-2)],
-                      check_val_every_n_epoch=2, benchmark=True)
+
+    #model = OneDNet.load_from_checkpoint(channel_count=len(included_channels),
+    #                                     included_classes=included_classes,
+    #                                     checkpoint_path="./lightning_logs/version_118/checkpoints/epoch=59-step=17640.ckpt")
+
+    trainer = Trainer(gpus=-1, callbacks=[TQDMProgressBar(refresh_rate=100),
+                                          StochasticWeightAveraging(),
+                                          ModelCheckpoint(save_weights_only=False)],
+                      check_val_every_n_epoch=1, benchmark=True)
 
     trainer.fit(model, train_data, val_data)
 
-    trainer.test(model, val_data)
+    trainer.test(model, test_data)
 
 
 if __name__ == "__main__":
