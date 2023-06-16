314ad7279cc25126803c1dc4f01c693b01ef40e5
theATM <the_atm@wp.pl>
2023-06-16 20:38:26 +0200

    BugFixes

--- Demo/server_demo.py
+++ Demo/server_demo.py
@@ -11,7 +11,7 @@ import time
 import threading
 from Server.server_params import *
 from Utilities.decision_making import DecisionMaker
-from Jetbot.utils.midas_detection import Midas, MidasInterpreter, DecisionMerger
+from Jetbot.utils.midas_detection import Midas, MidasInterpreter
 from Jetbot.utils.communication import FrameClient, CommandClient
 from Jetbot.utils.configuration import COMMANDS, INVCOMMANDS
 from pynput import keyboard
@@ -33,7 +33,7 @@ EEGKEY = None
 JETBOT_ADDRESS = '192.168.193.149'
 JETBOT_PORT = 3333
 
-SERVER_ADDRESS = 'localhost'
+SERVER_ADDRESS = '192.168.193.135'
 SERVER_PORT = 22243
 
 COMMAND_SERVING_PORT = 22242
@@ -44,6 +44,40 @@ free_boxes_lock = threading.Lock()
 KEYS = [False,False,False]
 EEGKEY = None
 listener = None
+
+class DecisionMerger:
+    def __init__(self,print=True):
+        self.przeszkoda = 0
+        self.print = print
+
+    # Requires usage of a lock with free_boxes_lock:
+    def merge(self, command, free_boxes):
+        left  = free_boxes[0]
+        front = free_boxes[1]
+        right = free_boxes[2]
+
+        if command == 'forward':
+            if front:
+                if self.print:
+                    print("Robot jedzie do przodu")
+                self.przeszkoda = 0
+                return 'forward'
+            else:
+                if self.print:
+                    print("Wykryto przeszkodę Robot stoi")
+        elif command == 'left':
+            if self.print:
+                print("Robot skreca w lewo")
+            self.przeszkoda = 0
+            return 'left'
+        elif command == 'right':
+            if self.print:
+                print("Robot skreca w prawo")
+            self.przeszkoda = 0
+            return 'right'
+        else:
+            return None
+
 
 def create_sockets():
     # # TCP Socket for receiving data from Actiview
@@ -67,9 +101,9 @@ def load_mean_std():
 
 def load_model():
     """Loading the trained OneDNet model"""
-    model = OneDNet.load_from_checkpoint(checkpoint_path="Modele/Kuba/Multi/model.ckpt", included_classes=[0, 1, 2],
-                                          channel_count=3)
-    #model = torch.load("Demo/JointNikodemKuba_multiclass.pt")
+    #model = OneDNet.load_from_checkpoint(checkpoint_path="Modele/Kuba/Multi/model.ckpt", included_classes=[0, 1, 2],
+    #                                      channel_count=3)
+    model = torch.load("Demo/JointNikodemKuba_multiclass.pt")
     model.eval()
     return model
 
@@ -89,7 +123,7 @@ def obstacle_detection():
         depth_image = midas.predict(frame,False,0,0)
         if depth_image is None:
             continue
-        new_free_boxes = midas_iterpreter.find_obstacles(depth_image)
+        new_free_boxes = midas_iterpreter.find_obstacles(depth_image,print_debug=False)
         if new_free_boxes is None:
             continue
         with free_boxes_lock:
@@ -116,16 +150,17 @@ class DataPicker():
         else:
             raise ValueError("mode must be either 'train', 'val' or 'test'")
 
-        self.dataset = {'data': data,
-                        'labels': labels}
+        self.dataset = {'data':  torch.from_numpy(data),
+                        'labels': torch.from_numpy(labels)}
         self.prepare_queues()
 
     def __call__(self, label):
         if label not in ['left', 'right', 'relax']:
-            raise ValueError('label should be either left, right or relax')
+            #raise ValueError('label should be either left, right or relax')
+            return None
         data = self.queues[label][0]
         self.queues[label] = np.roll(self.queues[label], -1, axis=0)
-        return data
+        return torch.Tensor(data).unsqueeze(dim=0)
 
     def prepare_queues(self):
         labels = self.dataset['labels']
@@ -155,6 +190,7 @@ def main():
     predicting.start()
     decision_merger = DecisionMerger()
     decision_source = 'left'
+    is_frame_data : bool = False
 
     while True:
         number_label = get_eeg_command()
@@ -162,33 +198,46 @@ def main():
         if label is not None:
             decision_source = label
 
-        if decision_source != 'neither':
-            x = data_picker(decision_source)
+        x = data_picker(decision_source)
+        if x is not None:
             y = dc.get_classification(x, model)
             out_ind = np.argmax(y.numpy())
             decision_maker.add_data(out_ind)
+            is_frame_data = True
 
         if time.time() - time_start > 0.75:
-            if decision_source != 'neither':
-                decision = str(decision_maker.decide())
-            #check if keyboard override
+            # DO the prediction:
+            # check if keyboard override
             keyboard_command = get_command()
             if keyboard_command is not None:
                 decision = keyboard_command
-            elif keyboard_command is None and decision_source == 'neither':
-                time_start = time.time()
-                continue
+            else:
+                # Check if there is new data to process:
+                if is_frame_data:
+                    # Detect the EEG signals:
+                    decision = str(decision_maker.decide())
+                else:
+                    # No new data - standby
+                    time_start = time.time()
+                    is_frame_data = False
+                    continue
+
+            is_frame_data = False
+            # Check weather ignoring the decision
             if decisions_to_ignore > 0 and prev_decision != decision and decision_ignored != decision:
+                # Ignoring
                 decisions_to_ignore -= 1
             else:
+                # using the decision
                 if decision == '0' or decision == '1':
                     decisions_to_ignore = 3
                     decision_ignored = decision
-                print(f"Decision: {kierunki[decision]}")
+                print(f"EEG Decision: {kierunki[decision]}")
                 print(decision_maker.decisions_masks)
                 if decision is not None and decision != 'None':
                     with free_boxes_lock:
                         decision = decision_merger.merge(COMMANDS[int(decision)],free_boxes)
+                # Sending the control command to the robot
                 if decision is not None and decision != 'None':
                     bytes_to_send = str.encode(str(INVCOMMANDS[decision]))
                     udp_server_sock.sendto(bytes_to_send, (JETBOT_ADDRESS, JETBOT_PORT))
@@ -202,21 +251,23 @@ def main():
 def get_command():
     # Check for arrow key input
     if KEYS[2]:
-        print('Up arrow key pressed')
+        #print('Up arrow key pressed')
         return '2'
     elif KEYS[0]:
-        print('Left arrow key pressed')
+        #print('Left arrow key pressed')
         return '0'
     elif KEYS[1]:
-        print('Right arrow key pressed')
+        #print('Right arrow key pressed')
         return '1'
     return None
 
 def get_eeg_command():
     # Check for 0 1 2 3 key input
+    y = EEGKEY
     if EEGKEY is not None:
-        print("Pressed Key " + str(EEGKEY))
-    return EEGKEY
+        #print("Pressed Key " + str(y))
+        y = str(y)
+    return y
 
 class SignalTypes(Enum):
     Letf = 0
